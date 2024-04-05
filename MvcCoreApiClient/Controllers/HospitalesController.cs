dcb48e65e28aff99ba449401bc046232a4d04e19
RazvanLucacii <rlucaci48@gmail.com>
2024-04-05 14:19:12 +0200

    Add project files.

---
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreApiClient.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
